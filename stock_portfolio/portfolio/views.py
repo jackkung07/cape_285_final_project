005dae02430692debc0c60dac7cacb2d06aac68a
ivanybma <ivanybma@yahoo.com>
2017-05-13 14:38:21 -0700

    add dummy data

--- stock_portfolio/portfolio/views.py
+++ stock_portfolio/portfolio/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import redirect
 from .models import UserCreationForm
+from yahoo_finance import Share
 from django.http import JsonResponse
 from django.http.response import HttpResponse
 
@@ -26,6 +27,15 @@ def user_registration_web(request):
 
 @login_required()
 def home_page(request):
+    rstlist = list()
+    apple = Share('APPL')
+    adobe = Share('ABDE')
+    nestle = Share('NSRGY')
+    vti = Share('VTI')
+    ixus = Share('IXUS')
+    iltb = Share('ILTB')
+
+
     if request.method == 'POST':
         allotment = int(str(request.POST['allotment']))
         ethical_investment = False
@@ -34,15 +44,28 @@ def home_page(request):
         quality_investment = False
         value_investment = False
         if 'ethical' in request.POST.keys():
+            data = {}
+            data['sticker'] = 'ADBE'
+            data['name'] = adobe.get_name()
+            data['price'] = adobe.get_price()
+            rstlist.append(data)
+            data['sticker'] = 'APPL'
+            data['name'] = apple.get_name()
+            data['price'] = apple.get_price()
+            rstlist.append(data)
             ethical_investment = True
+
         if 'growth' in request.POST.keys():
             growth_investment = True
         if 'index' in request.POST.keys():
+            rstlist.append(vti.get_name())
+            rstlist.append(iltb.get_name())
+            rstlist.append(ixus.get_name())
             index_investment = True
         if 'quality' in request.POST.keys():
             quality_investment = True
         if 'value' in request.POST.keys():
             value_investment = True
 
-        return render(request, 'portfolio/home.html', {'portfolio_data': None})
+        return render(request, 'portfolio/home.html', {'result': rstlist})
     return render(request, 'portfolio/home.html')
